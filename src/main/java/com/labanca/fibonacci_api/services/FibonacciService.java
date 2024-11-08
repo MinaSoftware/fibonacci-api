53a52d82563560c6d77910b21a37f8b45f360192
MinaSoftware <romi.fernandez.088@gmail.com>
2024-11-07 21:15:31 -0300

    Add endpoint and logic to get calculate fibonacci

---
+++ src/main/java/com/labanca/fibonacci_api/services/FibonacciService.java
@@ -0,0 +1,32 @@
+package com.labanca.fibonacci_api.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FibonacciService {
+
+    public ResponseEntity<String> getFibonacci(int position) {
+
+        if( position < 0) {
+            return new ResponseEntity<>("Negative position not allowed", HttpStatus.PRECONDITION_FAILED);
+        }
+        Integer result = calculateFibonacci(position);
+
+        //add exception
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    }
+
+    private int calculateFibonacci(int position) {
+        int previous = 0;
+        int current = position <= 1 ? position : 1;
+
+        for (int i = 2; i <= position; i++) {
+            int next = previous + current;
+            previous = current;
+            current = next;
+        }
+        return current;
+    }
+}
